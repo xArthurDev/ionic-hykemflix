d2fb444c193b0f2ae4cfa268c7e9edb05b1f20d6
xArthurDev <arthurrguimaraes12@gmail.com>
2020-05-08 15:47:00 -0300

    Added movies page

---
+++ src/app/movies/movies.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.page.html',
+  styleUrls: ['./movies.page.scss'],
+})
+export class MoviesPage implements OnInit {
+  slidesOptions = {
+    slidesPerView: 3
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
