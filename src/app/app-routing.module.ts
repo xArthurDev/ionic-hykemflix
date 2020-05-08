c32491618ddb85593ab6c9e68a93b8f65f85f071
xArthurDev <arthurrguimaraes12@gmail.com>
2020-05-08 15:42:11 -0300

    Added new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,38 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'movies',
+    loadChildren: () => import('./movies/movies.module').then( m => m.MoviesPageModule)
+  },
+  {
+    path: 'series',
+    loadChildren: () => import('./series/series.module').then( m => m.SeriesPageModule)
+  },
+  {
+    path: 'animes',
+    loadChildren: () => import('./animes/animes.module').then( m => m.AnimesPageModule)
+  },
+  {
+    path: 'favorites',
+    loadChildren: () => import('./favorites/favorites.module').then( m => m.FavoritesPageModule)
+  },
+  {
+    path: 'releases',
+    loadChildren: () => import('./releases/releases.module').then( m => m.ReleasesPageModule)
+  },
+  {
+    path: 'mwatched',
+    loadChildren: () => import('./mwatched/mwatched.module').then( m => m.MwatchedPageModule)
+  },
+  {
+    path: 'upcoming',
+    loadChildren: () => import('./upcoming/upcoming.module').then( m => m.UpcomingPageModule)
   }
 ];
 
