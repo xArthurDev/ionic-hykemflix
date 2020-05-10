61c53d0ffe568b54ed6a96b97d4bb23c8aa968f3
xArthurDev <arthurrguimaraes12@gmail.com>
2020-05-10 13:25:07 -0300

    Added modal movies and ngx embed in folder page

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { EmbedVideoService } from 'ngx-embed-video';
+import { ModalController } from '@ionic/angular';
+import { BrightComponent } from '../filmes/bright/bright.component';
+import { Milagre7Component } from '../filmes/milagre7/milagre7.component';
+import { SilenciopantComponent } from '../filmes/silenciopant/silenciopant.component';
+import { ThekingComponent } from '../filmes/theking/theking.component';
 
 @Component({
   selector: 'app-folder',
@@ -7,6 +13,12 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
+
+  youtubeUrl = 'https://www.youtube.com/watch?v=iHhcHTlGtRs';
+  youtubeId = 'iHhcHTlGtRs';
+
+
+
   slidesOptions = {
     slidesPerView: 3
   };
@@ -15,10 +27,44 @@ export class FolderPage implements OnInit {
   };
   public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private embedService: EmbedVideoService, private activatedRoute: ActivatedRoute, private ModalCtrl: ModalController) {
+    console.log(this.embedService.embed(this.youtubeUrl));
+    console.log(this.embedService.embed_youtube(this.youtubeId));
+   
+   }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+  }
+
+  //Filmes dentro de modais
+
+  async bright() {
+    const modal = await this.ModalCtrl.create({
+      component: BrightComponent
+    });
+    modal.present();
+  }
+
+  async milagre7() {
+    const modal = await this.ModalCtrl.create({
+      component: Milagre7Component
+    });
+    modal.present();
+  }
+
+  async silenciopant() {
+    const modal = await this.ModalCtrl.create({
+      component: SilenciopantComponent
+    });
+    modal.present();
+  }
+
+  async theking() {
+    const modal = await this.ModalCtrl.create({
+      component: ThekingComponent
+    });
+    modal.present();
   }
 
 }
